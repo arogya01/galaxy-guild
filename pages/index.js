20ee42b0c785085432c8e95a1b15ffc685160356
arogya <arogyabichpuria16@gmail.com>
2022-03-12 22:53:13 +0530

    removed the default font-bold prop from index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import CardsWrapper from '../components/CardsWrapper'
 import HeroSection from '../components/HeroSection'
 import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className='font-bold'>
+    <>
      <Navbar />
      <HeroSection />
-    </div>
+     
+    </>
   )
 }

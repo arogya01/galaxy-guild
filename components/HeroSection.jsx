615885d1d628a76d5ba6967f46dac6d973fe1548
arogya <arogyabichpuria16@gmail.com>
2022-03-12 15:47:55 +0530

    added the cards section to the hero

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -1,20 +1,32 @@
 import Image from 'next/image';
 import Bg from '../public/bgpic.png';
 import GlowingBtn from '../utils/glowingBtn';
+import CardsWrapper from './CardsWrapper';
+
 
 
 const BackgroundImg = () => {
     return(
         <div className='absolute -z-1 w-full h-full'>
         <Image src={Bg}  layout="fill" objectFit='cover' alt="bg"  />
+        <style jsx>{
+            `
+            div{
+                z-index:-1;
+            }
+                
+            `
+        }
+        </style>
         </div>
     )
 }
 
 export default function HeroSection (){
     return(
-        <div className='relative  h-[932px]'>
+        <div className='relative z-5 h-[932px]'>
         <BackgroundImg />
+        <div className=" pb-16">
         <section className=" z-5 text-white p-8 text-center">
         <h1 className='text-3xl leading-7 pb-6'>BRING THE GAME</h1>
         <p className='font-extralight text-lg leading-8 '>Galaxy Guild serves as the platform for Players, Guilds, and Scholarship Managers to collaborate and make the most out of their Metaverse experience! </p>
@@ -24,6 +36,10 @@ export default function HeroSection (){
         <GlowingBtn btnName="Read Whitepaper"/>
 
         </div>
+
+        </div>
+
+        <CardsWrapper />
         </div>
     )
 }
